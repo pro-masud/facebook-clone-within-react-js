39385aea675a507921eefe081cf063b25534da82
pro-masud <promasudbd@gmail.com>
2024-05-31 19:55:00 +0600

    Faceb User Interface Design Complete

--- src/components/meta/Meta.jsx
+++ src/components/meta/Meta.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet";
 
 const Meta = ({title}) => {
-  return <Helmet >
+  return <Helmet>
             <title>{title}</title>
             <link rel="icon" type="image/svg+xml" href="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/512px-2021_Facebook_icon.svg.png" />
         </Helmet>
--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,11 +1,33 @@
+import { Link } from "react-router-dom";
 import Meta from "../../components/meta/Meta";
+// import "./../../index.scss";
 import "./auth.scss";
 
 const Auth = () => {
   return (
     <>
       <Meta title="Facebook log in or sing up" />
-      <h1>Auth</h1>
+      <div className="fb-auth-wrapper">
+        <div className="fb-auth-container">
+          <div className="fb-auth-left">
+            <img src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt=".logo" className="auth-fb-logo" />
+            <h2 className="fb-auth-left-text">Facebook helps you connect and share with the people in your life.</h2>
+          </div>
+          <div className="fb-auth-right">
+            <div className="fb-auth-form-box">
+              <form className="fb-auth-form">
+                <input type="text" placeholder="Email Address or Phone Number"/>
+                <input type="password" placeholder="Password"/>
+                <button className="fb-btn log-btn">Log In</button>
+              </form>
+              <Link className="fb-forget-btn" to="">Forgotten password?</Link>
+              <div className="divider"></div>
+              <button className="btn-create">Create new account</button>
+            </div>
+           <p className="fb-auth-text"><Link to="">Create a Page</Link> for a celebrity, brand or business.</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
